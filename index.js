2aa767af9189e9b4702ab28a7a6ef61fdf9ce131
Vidaz3 <osahondave2002@gmail.com>
2022-06-23 20:48:18 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let myName="Osahon Davis"
+let myHeight="178cm"
+let country="Nigeria"
+document.write("My name is :"+ " " + myName +"<br>"+"My height is:" +" "+ myHeight +"<br>"+"My country of origin is:" + " " +country)
